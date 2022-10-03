76d189d521aacb94cb03d48e893ce2b5a4ee92fc
Marcelo Rodrigo Duran Mamani <103041686+Marcelo-Duran-M-21@users.noreply.github.com>
2022-10-03 02:58:18 +0200

    Agregar archivos de proyecto.

---
+++ LAB0101/MYbiografia/MYbiografia/MainSource.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int var;
+    while (true) {
+        cout << "\t\tHola Mi nombre es Marcelo pero todos me dicen (chelo)\n Soy un amante de la Tecnologia como de la progrmacion que es un gran herramienta que nos ayuda hacer mejor la vida \n";
+        cout << "Este semestre me comprometo no Aplazarme en la materia se que tengo la capacidad de aprobar por ello he de seguir \n cada fraso para mi es una leccion por ello los fracasos me ensenan mucho como no a volver a cometer el mismo herror\n ";
+        cout << "\n\n";
+
+        cout << "\t\t\thola mundo";
+
+        cout << "\n\n";
+        cout << "Quiere seguir leendo la resena o desea salir salir (1) seguir (0)?";
+        cin >> var;
+        if (var == 1) {
+            cout << "bay bay bay   :)    ";
+
+        }
+        else {
+            break;
+
+
+        }
+    }
+    return 0;
+}
